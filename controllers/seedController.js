f533832e05a89acfacddaca7239972efae8ef9a2
Chris Poulter <cpoulter@hotmail.co.uk>
2018-10-22 09:09:25 +0100

    code clean up

--- controllers/seedController.js
+++ controllers/seedController.js
@@ -3,9 +3,9 @@ const config = require('../utils/config');
 const User = require('../models/user');
 
 exports.seedData = async (req, res) => {
-    var administrator = {
+    var user = {
         emailAddress: config.SEED_EMAIL_ADDRESS,
-        password: config.SEED_PASSWORD,
+        password: await password.hash(config.SEED_PASSWORD),
         firstName: 'System',
         lastName: 'Administrator',
         dateOfBirth: '1970-01-01',
@@ -13,18 +13,14 @@ exports.seedData = async (req, res) => {
     };
 
     const existing = await User.findOne({
-        emailAddress: administrator.emailAddress
+        emailAddress: user.emailAddress
     });
 
     if (existing) {
         await existing.remove();
     }
 
-    const newPassword = await password.hash(administrator.password);
-
-    const newUser = new User(administrator);
-    newUser.password = newPassword;
-    await newUser.save();
+    await new User(user).save();
 
     return res.send('Database seeded.');
 };
