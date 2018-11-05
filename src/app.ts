81c10382f80dc70412f073386a72734fea2113c2
Chris Poulter <cpoulter@hotmail.co.uk>
2018-11-05 10:06:21 +0000

    code clean up

--- src/app.ts
+++ src/app.ts
@@ -65,6 +65,6 @@ app.use(errorMiddleware);
 
 app.set('port', process.env.PORT || 3001);
 
-const server = app.listen(app.get('port'), () => {
-    console.log(`Express server listening on port ${server.address()}`);
+app.listen(app.get('port'), () => {
+    console.log(`Express server listening on port ${app.get('port')}`);
 });
